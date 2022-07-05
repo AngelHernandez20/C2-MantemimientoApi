724e18766e5ffd2104429edded92a72ba4e91c0f
AngelHernandez20 <191280@ids.upchiapas.edu,mx>
2022-07-05 14:36:05 -0500

    MC-24-POST

--- api/views.py
+++ api/views.py
@@ -1,11 +1,21 @@
+import json
+
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import  Agentes
 from .serializers import AgentesSerializer
 from rest_framework import viewsets
 from django.views import View
 from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
 
 class agentesView(View):
+
+        @method_decorator(csrf_exempt)
+        def dispatch(self, request, *args, **kwargs):
+            return super().dispatch(request, *args, **kwargs)
+
         def get(self, request):
             agentes = list(Agentes.objects.values())
             if len(agentes)>0:
@@ -15,7 +25,12 @@ class agentesView(View):
             return JsonResponse(datos)
 
         def post(self, request):
-            pass
+            jd= json.loads(request.body)
+            print(jd)
+            Agentes.objects.create(nombre=jd['nombre'], rol=jd['rol'], pais=jd['pais'], habilidad_q=jd['habilidad_q'], habilidad_e=jd['habilidad_e'],
+                                   habilidad_c=jd['habilidad_c'], definitiva=jd['definitiva'])
+            datos = {'message':"Succes"}
+            return JsonResponse(datos)
 
         def put(self, request):
             pass
